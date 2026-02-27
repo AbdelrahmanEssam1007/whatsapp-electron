7590e79d00360e79f4479a9d36088d3dacb5f337
Abdelrahman <70455708+AbdelrahmanEssam1007@users.noreply.github.com>
2026-02-27 13:14:34 +0200

    bugfix(tray) fixed a bug where the closing the app doesn't kill the process and the tray icon persists in kde plasma

--- src/tray.js
+++ src/tray.js
@@ -1,17 +1,21 @@
-const { Tray, Menu } = require("electron");
+const { Tray, Menu, app } = require("electron");
 
 module.exports = function setupTray(win, trayIconPath) {
   const tray = new Tray(trayIconPath);
 
   const menu = Menu.buildFromTemplate([
     { label: "Show WhatsApp", click: () => { win.show(); win.focus(); } },
-    { label: "Quit", click: () => { process.exit(0); } }
+    { label: "Quit", click: () => { app.quit(); } }
   ]);
 
   tray.setToolTip("WhatsApp");
   tray.setContextMenu(menu);
 
-  tray.on("click", () => win.isVisible() ? win.hide() : win.show());
+  tray.on("click", () => {
+    if (win && !win.isDestroyed()) {
+      win.isVisible() ? win.hide() : win.show();
+    }
+  });
 
   return tray;
 };
